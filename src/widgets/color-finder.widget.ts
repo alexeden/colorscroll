d26bd71baaf17a83776c699707d6d120b617491f
alexeden <AlexanderEden91@gmail.com>
2017-07-11 14:08:59 -0500

    Adding color-details widget

---
+++ src/widgets/color-finder.widget.ts
@@ -0,0 +1,40 @@
+import { Observable } from 'rxjs';
+import { Selectors, replaceContentAtSelector, htmlFromString, observeInConsole, writeToSelector } from '../shared';
+import { ColourLovers } from '../api';
+import { hexString$ } from '../features';
+
+const widgetSelectors = {
+  colorName: '.color-name'
+};
+
+const hex$ = hexString$.debounceTime(2000).share();
+
+// const colorDetails$ = hex$.switchMap(ColourLovers.getColor);
+
+const colorName$
+  = hex$
+      .switchMap(hex =>
+        Observable.concat(
+          Observable.of('Getting color details'),
+          ColourLovers.getColor(hex)
+            .map(colors =>
+              colors.length > 0
+                ? colors[0].title
+                : 'Color not found'
+            )
+        )
+      );
+
+
+colorName$
+  .do(iconText => writeToSelector(widgetSelectors.colorName, iconText))
+  .subscribe(observeInConsole('hsv'));
+
+const colorDetailsElement = htmlFromString(`
+  <h1 class="color-name"></h1>
+
+`);
+
+
+console.log(colorDetailsElement);
+export const ColorFinderWidget = replaceContentAtSelector(Selectors.colorDetailsWidget, colorDetailsElement);
