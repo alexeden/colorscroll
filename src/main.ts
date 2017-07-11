f41fbfb97550612dc58647b450f2f6abbbf2a635
alexeden <AlexanderEden91@gmail.com>
2017-07-11 14:09:12 -0500

    Moved variable globalization to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import * as Rx from 'rxjs';
 import { Observable } from 'rxjs';
-import { writeToSelector, observeInConsole, Accumulators } from './helpers';
-import { changingHue1$, changingHue2$, hue$, sat$, light$, hslString$ } from './features';
+import { Selectors, writeToSelector, observeInConsole } from './shared';
+import { changingHue1$, changingHue2$, hue$, sat$, light$, hslString$, hexString$ } from './features';
+import { ColorFinderWidget } from './widgets';
 
+/* Globalize helpers */
+window['observeInConsole'] = observeInConsole;
+window['writeToSelector'] = writeToSelector;
+
+/* Globalize RxJS */
+window['Rx'] = Rx;
+Object.entries(Rx).map(([prop, value]) => window[prop] = value);
+
+
+console.log(ColorFinderWidget);
 /*
+  - Overview of HSL
+
+
   Feature #1  Color-changing icons
   - Operators: Observable.interval, .map, .do
   - Helper introduction: writeToSelector
@@ -22,8 +37,8 @@ import { changingHue1$, changingHue2$, hue$, sat$, light$, hslString$ } from './
 
 /* Create the color-changing icons */
 // Icon DOM elements
-const icon1 = document.querySelector('#color-scroll-icon-1') as HTMLElement;
-const icon2 = document.querySelector('#color-scroll-icon-2') as HTMLElement;
+const icon1 = document.querySelector(Selectors.icon1) as HTMLElement;
+const icon2 = document.querySelector(Selectors.icon2) as HTMLElement;
 
 // Icon DOM element clicks
 const iconClicks1$ = Observable.fromEvent(icon1, 'click');
@@ -34,10 +49,26 @@ changingHue1$.takeUntil(iconClicks1$).subscribe(hue => icon1.style.color = `hsl(
 changingHue2$.takeUntil(iconClicks2$).subscribe(hue => icon2.style.color = `hsl(${hue}, 100%, 50%)`);
 
 
-hue$.subscribe(writeToSelector('.hue-value'));
-sat$.subscribe(writeToSelector('.saturation-value'));
-light$.subscribe(writeToSelector('.lightness-value'));
+hue$.subscribe(hue => writeToSelector(Selectors.hueValue, hue));
+sat$.subscribe(sat => writeToSelector(Selectors.saturationValue, sat));
+light$.subscribe(light => writeToSelector(Selectors.lightnessValue, light));
 
+//
 hslString$
-  .do(writeToSelector('#hsl-string'))
+  .map(hslString => writeToSelector(Selectors.hslString, hslString))
   .subscribe(hsl => document.body.style.backgroundColor = hsl);
+
+hexString$.subscribe(hexString => writeToSelector(Selectors.hexString, hexString));
+
+
+/*
+  Future features:
+  - Holding the shift key decreases the scroll sensitivity for fine-tuned scrolling
+  - Create a toggle button that changes the scroll control zones between HSL and RGB modes
+  - Icon starts changing colors again after having previously been stopped
+  - Double-clicking a value displays an input that allows you to enter a numeric value manually
+  - Keyboard-only controls (tab key to switch control zones, arrow keys to change value)
+
+
+
+*/
