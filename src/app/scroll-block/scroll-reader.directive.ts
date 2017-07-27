576e5fdaeec6f995189e6335351be9e1a523de8d
alexeden <AlexanderEden91@gmail.com>
2017-07-26 22:10:00 -0500

    FInalized the scroll-reader directive

--- src/app/scroll-block/scroll-reader.directive.ts
+++ src/app/scroll-block/scroll-reader.directive.ts
@@ -1,36 +1,18 @@
 import { Observable, Subject } from 'rxjs';
 import { Directive, ElementRef, Input, Output, OnInit, OnDestroy } from '@angular/core';
 
-export enum ScrollAxis {
-  X = 'x',
-  Y = 'y'
-}
-
-export interface ScrollReaderEvent {
-  axis: ScrollAxis;
-  delta: number;
-  shiftKey: boolean;
-}
-
-
 @Directive({
   selector: '[scrollReader]'
 })
 export class ScrollReaderDirective implements OnInit, OnDestroy {
 
-  @Input('scrollReader') axis = ScrollAxis.Y;
+  @Input('scrollReader') axis: 'x'|'y' = 'x';
   @Input() throttle = 1;
   @Input() throttleWithShift = 100;
-  @Input() clampRange = 1;
-  @Input() clampRangeWithShift = 1;
   @Output() scroll = new Subject<number>();
 
   private scrollEvents$: Observable<MouseWheelEvent>;
   private unsubscribe$ = new Subject<string>();
-
-  static clamp(min: number, max: number, value: number): number {
-    return Math.max(min, Math.min(value, max));
-  }
 
   constructor(
     private elementRef: ElementRef
@@ -47,10 +29,9 @@ export class ScrollReaderDirective implements OnInit, OnDestroy {
     this.scrollEvents$
       .exhaustMap(event => {
         const throttle = event.shiftKey ? this.throttleWithShift : this.throttle;
-        const delta = this.axis === ScrollAxis.X ? event.deltaX : event.deltaY;
-        const clamp = event.shiftKey ? this.clampRangeWithShift : this.clampRange;
-        const clampedDelta = ScrollReaderDirective.clamp(-clamp, clamp, delta);
-        return Observable.of(clampedDelta).delay(throttle);
+        const delta = this.axis === 'x' ? event.deltaX : event.deltaY;
+        const normalDelta = delta < 0 ? -1 : 1;
+        return Observable.of(normalDelta).delay(throttle);
       })
       .takeUntil(this.unsubscribe$)
       .subscribe(this.scroll);
