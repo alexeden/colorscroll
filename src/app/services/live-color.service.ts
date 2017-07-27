c5961fcea75169e1adac5f906130d6289e5fba30
alexeden <AlexanderEden91@gmail.com>
2017-07-26 23:32:16 -0500

    Added a hex$ stream to the live color service

--- src/app/services/live-color.service.ts
+++ src/app/services/live-color.service.ts
@@ -2,29 +2,23 @@ import { Injectable } from '@angular/core';
 import { Subject, ConnectableObservable, Observable } from 'rxjs';
 import { HSL } from '../../shared';
 import { TheColorApiService } from '../../api';
+import { ColorConverterService } from './color-converter.service';
 
 export type HslOperation = (color: HSL) => HSL;
 
 @Injectable()
 export class LiveColorService {
   hsl$: ConnectableObservable<HSL>;
+  hex$: Observable<string>;
   hslString$: Observable<string>;
 
   private readonly hslUpdates$ = new Subject<HslOperation>();
 
-  static circular(min: number, max: number, value: number): number {
-    return value <= min
-      ? value + max
-      : value % max;
-  }
-
-  static clamp(min: number, max: number, value: number): number {
-    return Math.max(min, Math.min(value, max));
-  }
-
   constructor(
-    public colorApi: TheColorApiService
+    public colorApi: TheColorApiService,
+    private converter: ColorConverterService
   ) {
+    window['convert'] = this.converter;
     this.hsl$
       = this.hslUpdates$
           .startWith((hsl: HSL) => hsl)
@@ -35,7 +29,7 @@ export class LiveColorService {
           .publishReplay(1);
 
     this.hslString$ = this.hsl$.map(({h, s, l}) => `hsl(${h}, ${s}%, ${l}%)`);
-
+    this.hex$ = this.hsl$.map(({h, s, l}) => this.converter.hsl.hex([h, s, l]));
     this.hsl$.connect();
   }
 
