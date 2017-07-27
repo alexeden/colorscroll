1576712564f101c3e87cb1739e2f888140d4e27b
alexeden <AlexanderEden91@gmail.com>
2017-07-26 23:33:20 -0500

    Deleted the old features

--- src/features/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './color.stream';
-export * from './background-gradient';
-export * from './color-change-icon';
-export * from './color-control-scrolls';
-export * from './hsl-values';
-export * from './color-css-values';
